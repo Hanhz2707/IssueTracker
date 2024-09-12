e347b349ed8ea6b2c4ca47e4414c62c840419bf8
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-09-12 12:54:02 +0300

    Set up RadixUI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <>
+      <div className="ml-1">IssuesPage</div>
+      <Button>Click me</Button>
+    </>
+  );
 };
 
 export default IssuesPage;
