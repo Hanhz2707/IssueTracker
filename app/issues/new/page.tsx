5ccb47e20bf019602a4b5c80de68373ca1469abf
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-09-12 21:38:19 +0300

    Extracting Errormessagfe compo

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CreateIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof CreateIssueSchema>;
 
@@ -56,11 +57,9 @@ const NewIssuePage = () => {
             placeholder="Title"
             {...register("title")}
           ></TextField.Root>
-          {errors.title && (
-            <Text color="red" as="p">
-              {errors.title.message}
-            </Text>
-          )}
+
+          <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
           <Controller
             name="description"
             control={control}
@@ -68,11 +67,8 @@ const NewIssuePage = () => {
               <SimpleMDE placeholder="Type something here..." {...field} />
             )}
           />
-          {errors.description && (
-            <Text color="red" as="p">
-              {errors.description.message}
-            </Text>
-          )}
+
+          <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
           <Button>Submit</Button>
         </form>
