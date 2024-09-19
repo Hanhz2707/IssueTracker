9ec31f4cc5bbb5eb560e12563aeb237aafacde76
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-09-19 20:25:54 +0300

    Show a confirmation deleteion

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,10 +1,34 @@
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 const DeleteIssueButton = ({ IssueID }: { IssueID: number }) => {
   return (
     <>
-      <Button color="red"> Delete Issue</Button>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Confirm Deleteion</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure? This application will no longer be accessible and any
+            existing sessions will be expired.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
     </>
   );
 };
