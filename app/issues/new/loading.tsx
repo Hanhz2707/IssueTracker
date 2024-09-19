a15fac6cfb7df4c2c2b9acaf859388c51ec49ac4
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-09-19 19:16:20 +0300

    Improve loading ux

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,14 +1,2 @@
-import Skeleton from "@/app/components/Skeleton";
-import { Box } from "@radix-ui/themes";
-
-const LoadingNewIssuePage = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton height={"2rem"} />
-
-      <Skeleton height={"20rem"} />
-    </Box>
-  );
-};
-
-export default LoadingNewIssuePage;
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+export default IssueFormSkeleton;
